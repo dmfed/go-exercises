79fa02fe37500b4ae15027881cef72b7f234ee32
dmfed <dafedotov@gmail.com>
2020-08-18 23:34:53 +0300

    Added new codewars exercise

---
+++ divisibility_by_thirteen-codewars.go
@@ -0,0 +1,70 @@
+/*
+When you divide the successive powers of 10 by 13 you get the following
+remainders of the integer divisions:
+
+1, 10, 9, 12, 3, 4.
+
+Then the whole pattern repeats.
+
+Hence the following method: Multiply the right most digit of the number
+with the left most number in the sequence shown above, the second right
+most digit to the second left most digit of the number in the sequence.
+The cycle goes on and you sum all these products. Repeat this process until
+the sequence of sums is stationary.
+
+...........................................................................
+
+Example: What is the remainder when 1234567 is divided by 13?
+
+7×1 + 6×10 + 5×9 + 4×12 + 3×3 + 2×4 + 1×1 = 178
+
+We repeat the process with 178:
+
+8x1 + 7x10 + 1x9 = 87
+
+and again with 87:
+
+7x1 + 8x10 = 87
+
+...........................................................................
+
+From now on the sequence is stationary and the remainder of 1234567 by 13
+is the same as the remainder of 87 by 13: 9
+
+Call thirt the function which processes this sequence of operations on an
+integer n (>=0). thirt will return the stationary number.
+
+thirt(1234567) calculates 178, then 87, then 87 and returns 87.
+
+thirt(321) calculates 48, 48 and returns 48
+*/
+
+package main
+
+import "fmt"
+
+var remainders = []int{1, 10, 9, 12, 3, 4}
+
+func Thirt(n int) int {
+	sum := 0
+	rindex := 0
+	tmp := n
+	for tmp > 0 {
+		r := tmp % 10
+		sum += r * remainders[rindex]
+		tmp /= 10
+		rindex = (rindex + 1) % len(remainders)
+	}
+	if sum == n {
+		return sum
+	}
+	return Thirt(sum)
+}
+
+func main() {
+	fmt.Println(Thirt(1234567))  // 87
+	fmt.Println(Thirt(8529))     // 79
+	fmt.Println(Thirt(85299258)) // 31
+	fmt.Println(Thirt(5634))     // 57
+	fmt.Println(Thirt(321))      // 48
+}
