4ada49719d278c15609db0865c3c0191c7e7b50f
Dmitry Fedotov <dafedotov@gmail.com>
2020-11-24 01:49:36 +0300

    Corrected mistake in maxHeap.go

--- maxHeap.go
+++ maxHeap.go
@@ -2,13 +2,10 @@ package main
 
 import "fmt"
 
-type MaxHeap struct {
-	nodes []int
-}
+type MaxHeap []int
 
 func NewMaxHeap(arr []int) MaxHeap {
 	var mh MaxHeap
-	mh.nodes = []int{}
 	for _, val := range arr {
 		mh.Insert(val)
 	}
@@ -16,19 +13,19 @@ func NewMaxHeap(arr []int) MaxHeap {
 }
 
 func (mh *MaxHeap) Insert(n int) {
-	mh.nodes = append(mh.nodes, n)
-	mh.siftUp(len(mh.nodes) - 1)
+	(*mh) = append((*mh), n)
+	mh.siftUp(len((*mh)) - 1)
 }
 
 func (mh *MaxHeap) GetMax() int {
-	return mh.nodes[0]
+	return (*mh)[0]
 }
 
 func (mh *MaxHeap) ExtractMax() int {
-	n := mh.nodes[0]
-	mh.nodes[0] = mh.nodes[len(mh.nodes)-1]
-	mh.nodes = mh.nodes[:len(mh.nodes)-1]
-	if len(mh.nodes) > 1 {
+	n := (*mh)[0]
+	(*mh)[0] = (*mh)[len((*mh))-1]
+	(*mh) = (*mh)[:len((*mh))-1]
+	if len((*mh)) > 1 {
 		mh.siftDown(0)
 	}
 	return n
@@ -36,8 +33,8 @@ func (mh *MaxHeap) ExtractMax() int {
 
 func (mh *MaxHeap) siftUp(i int) {
 	p := mh.parent(i)
-	for mh.nodes[i] > mh.nodes[p] {
-		mh.nodes[i], mh.nodes[p] = mh.nodes[p], mh.nodes[i]
+	for (*mh)[i] > (*mh)[p] {
+		(*mh)[i], (*mh)[p] = (*mh)[p], (*mh)[i]
 		i = p
 		p = mh.parent(i)
 	}
@@ -45,8 +42,8 @@ func (mh *MaxHeap) siftUp(i int) {
 
 func (mh *MaxHeap) siftDown(p int) {
 	ch := mh.maxChild(p)
-	for mh.nodes[p] < mh.nodes[ch] {
-		mh.nodes[p], mh.nodes[ch] = mh.nodes[ch], mh.nodes[p]
+	for (*mh)[p] < (*mh)[ch] {
+		(*mh)[p], (*mh)[ch] = (*mh)[ch], (*mh)[p]
 		p = ch
 		ch = mh.maxChild(p)
 	}
@@ -61,13 +58,13 @@ func (mh *MaxHeap) parent(ch int) int {
 
 func (mh *MaxHeap) maxChild(p int) int {
 	ch1, ch2, maxchild := p, p, p
-	if p*2+1 < len(mh.nodes)-1 {
+	if p*2+1 < len((*mh)) {
 		ch1 = p*2 + 1
 	}
-	if p*2+2 < len(mh.nodes)-1 {
+	if p*2+2 < len((*mh)) {
 		ch2 = p*2 + 2
 	}
-	if mh.nodes[ch2] > mh.nodes[ch1] {
+	if (*mh)[ch2] > (*mh)[ch1] {
 		maxchild = ch2
 	} else {
 		maxchild = ch1
@@ -76,18 +73,11 @@ func (mh *MaxHeap) maxChild(p int) int {
 }
 
 func main() {
-	a := []int{2, 9, 3, 6, 7, 10, 5}
+	a := []int{2, 3, 1, 4, 6, 5, 8, 10, 7, 9, 12, 11}
+
 	b := NewMaxHeap(a)
-	fmt.Println(b)
-	fmt.Println(b.GetMax())
-	b.Insert(11)
-	for len(b.nodes) > 1 {
+	for len(b) > 0 {
+		fmt.Println(b)
 		fmt.Println(b.ExtractMax())
 	}
-	fmt.Println(b.nodes)
-	fmt.Println(b)
-	c := NewMaxHeap([]int{1})
-	fmt.Println(c)
-	c.ExtractMax()
-	fmt.Println(c)
 }
